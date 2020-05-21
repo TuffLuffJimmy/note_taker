f534d5f5b87e92c8a4b4df4b65aac684d309f66f
Jimmy Echternach <jimmynechternach@gmail.com>
2020-05-21 16:53:29 -0700

    mid update - hope that I'm doing this correctly

---
+++ routes/listRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const Note = require('../Note.js')
+
+const note = new Note()
+
+router.get('./public/notes.html')
+
+router.post()
+
+router.put()
+
+router.delete()
+
+module.exports = router
