59d934ecb45bc50999240d436c1ef5b85ab628c6
Jimmy Echternach <jimmynechternach@gmail.com>
2020-05-21 19:54:52 -0700

    still broken, but perhaps slightly less broken

--- routes/listRoutes.js
+++ routes/listRoutes.js
@@ -1,16 +1,56 @@
+// allows us to use a separate routes file for cleaner code
 const router = require('express').Router()
+// constructor for reading and writing note files
 const Note = require('../Note.js')
-
+// json database
+const db = require('../db/db.json')
+// used to read and write json database
+const { readFile, writeFile } = require('fs')
+// promise based read and write files
+const { promisify } = require('util')
+const readFileSync = promisify(readFile)
+const writeFileSync = promisify(writeFile)
+// default construction for note object
 const note = new Note()
+const dbLocation = '../db/db.json'
+let noteId = 0
 
-router.get('./public/notes.html', (req, res) => {
-  
+// responds with json db
+router.get('./api/notes', (req, res) => {
+  console.log(req.body)
+  res.json(db)
 })
 
-router.post()
+router.post('./api/notes', (req, res) => {
+  console.log(req.body)
+  const title = req.body.title
+  const text = req.body.text
+  db.push(title,text,noteId)
+  noteId++
+  writeFileSync(dbLocation, JSON.stringify(db))
+    .then (res => console.log(res))
+    .catch(err => console.log(err))
 
-router.put()
+})
 
-router.delete()
+router.put('./api/notes', (req, res) => {
+
+})
+
+router.delete('./api/notes:id', (req, res) => {
+  db.splice(req.param.id,1)
+  // db.forEach((item) => {
+  //   if (item.id === parseInt(req.param.id))
+  // })
+  db.forEach((item) => {
+    if (item.id === parseInte(req.param.id)) {
+      db.pop(item)
+    }
+  })
+  writeFileSync(dbLocation, JSON.stringify(db))
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 // const http = require('http')
-const express = require('express')
-const { join } = require('path')
-const app = express()
 // const server = http.createServer()
 
-app.use(express.static(join(__dirname, 'public')))
+// server framework
+const express = require('express')
+// shorthand path writing tool
+const { join } = require('path')
+// allows us to use express, but with dot notation
+const app = express()
+
+
+// boilerplate? Should be in all express servers?
+app.use(express.static(join(__dirname, 'public'))) // this line takes us to the index page within public folder?
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-//app.use(require('./routes/listRoutes.js'))
+app.use(require('./routes/listRoutes.js'))
+
+// takes us to notes page within public folder
 app.get('/notes', (req, res) => {
   res.sendFile(join(__dirname, 'public', 'notes.html'))
 })
+
+// starts server
 app.listen(3000, () => console.log('http://localhost:3000'))
